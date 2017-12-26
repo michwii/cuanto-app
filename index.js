e87628cdae740d952a1f71aa0c8437cf1d1d5015
Elyes Hachem <freezerhm@yahoo.fr>
2017-12-26 21:29:14 +0100

    Adding Materialize CSS framework

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 var port = process.env.PORT || 1337;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log('Example app listening on port 3000!'))
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render("index")
+});
+
+app.listen(port, () => {
+  console.log('Example app listening on port ' + port)
+});
