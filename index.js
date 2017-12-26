9ca514e1d5c5a84a80a704848681dde6cc8303bf
Elyes Hachem <freezerhm@yahoo.fr>
2017-12-26 20:13:43 +0100

    with express

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-var http = require('http');
-
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
-});
-
+const express = require('express')
+const app = express()
 var port = process.env.PORT || 1337;
-server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log('Example app listening on port 3000!'))
